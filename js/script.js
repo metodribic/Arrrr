cf87738730cd0072f8c4f74b8538e69253996bc7
metodribic <metod.ribic@gmail.com>
2015-11-17 01:00:52 +0100

    Added terrain collision, texts, score counter

--- js/script.js
+++ js/script.js
@@ -20,6 +20,8 @@ var started = false;
 var score = 0;
 var divScore;
 var cube1;
+var colided = false;
+var finished = true;
  
 init();
 animate();
@@ -27,6 +29,7 @@ animate();
  
 function init() {
         container = document.getElementById( 'container' );
+        score = 0;
  
         //camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 20000 );
         camera = new THREE.PerspectiveCamera( 35, window.innerWidth / window.innerHeight, 1, 110000 );
@@ -131,7 +134,172 @@ function init() {
         cube.add(camera);
         scene.add(cube);
        
+
+        //############ CILJ ############
+        // instantiate a loader
+		var loader1 = new THREE.OBJLoader();
+
+		// // load a resource
+		// loader1.load(
+		// 	// resource URL
+		// 	'models/chest.obj',
+		// 	// Function when resource is loaded
+		// 	function ( chest ) {
+		// 		chest.position.y = water.position.y + 200     
+		//         chest.position.x = 250;
+		//         chest.position.z = -19000;
+		// 		scene.add( chest );
+		// 	}
+		// );
+        var geometryCube1 = new THREE.BoxGeometry(25, 25, 25);
+        var materialCube1 = new THREE.MeshBasicMaterial({color: 0xffd700});
+        finish = new THREE.Mesh(geometryCube1, materialCube1);
+        //voda je na 250 torej more bit objekt na 250+polovica višine objekta
+        finish.position.y = water.position.y + Math.round(finish.geometry.parameters.height/2);     
+        finish.position.x = 250;
+        finish.position.z = -19000;
+        scene.add(finish);
        
+		//############ TEREN-COLLISION ############
+        var y = water.position.y + Math.round(cube.geometry.parameters.height/2);
+ 
+        var pot = new THREE.SplineCurve3( [
+                new THREE.Vector3(373, y, 24840 ),
+                new THREE.Vector3(209, y, 22050 ),
+                new THREE.Vector3(197, y, 21408 ),
+                new THREE.Vector3(120, y, 21005 ),
+                new THREE.Vector3(93, y, 19689 ),
+                new THREE.Vector3(148, y, 18802 ),
+                new THREE.Vector3(140, y, 18282 ),
+                new THREE.Vector3(102, y, 17363 ),
+                new THREE.Vector3(115, y, 16535 ),
+                new THREE.Vector3(137, y, 14476 ),
+                new THREE.Vector3(114, y, 13331 ),
+                new THREE.Vector3(75, y, 12368 ),
+                new THREE.Vector3(17, y, 10905 ),
+                new THREE.Vector3(-19, y, 9597 ),
+                new THREE.Vector3(-18, y, 8562 ),
+                new THREE.Vector3(4, y, 7206 ),
+                new THREE.Vector3(41, y, 6958 ),
+                new THREE.Vector3(192, y, 5188 ),
+                new THREE.Vector3(-3, y, 3944 ),
+                new THREE.Vector3(-32, y, 3142 ),
+                new THREE.Vector3(-2, y, 1351 ),
+                new THREE.Vector3(41, y, -501 ),
+                new THREE.Vector3(87, y, -1690 ),
+                new THREE.Vector3(239, y, -1719 ),
+                new THREE.Vector3(197, y, -3715 ),
+                new THREE.Vector3(270, y, -6216 ),
+                new THREE.Vector3(337, y, -7206 ),
+                new THREE.Vector3(445, y, -8604 ),
+                new THREE.Vector3(531, y, -10415 ),
+                new THREE.Vector3(520, y, -12552 ),
+                new THREE.Vector3(552, y, -13973 ),
+                new THREE.Vector3(514, y, -14921 ),
+                new THREE.Vector3(453, y, -17007 ),
+                new THREE.Vector3(417, y, -17926 ),
+                new THREE.Vector3(376, y, -18430 ),
+                new THREE.Vector3(338, y, -19429 ),
+                new THREE.Vector3(205, y, -19943 ),
+                new THREE.Vector3(97, y, -19334 ),
+                new THREE.Vector3(23, y, -17685 ),
+                new THREE.Vector3(-9, y, -16203 ),
+                new THREE.Vector3(-62, y, -15608 ),
+                new THREE.Vector3(-81, y, -13808 ),
+                new THREE.Vector3(-103, y, -13050 ),
+                new THREE.Vector3(-70, y, -11672 ),
+                new THREE.Vector3(-51, y, -10525 ),
+                new THREE.Vector3(-49, y, -9143 ),
+                new THREE.Vector3(-10, y, -8224 ),
+                new THREE.Vector3(27, y, -7476 ),
+                new THREE.Vector3(57, y, -7018 ),
+                new THREE.Vector3(60, y, -6171 ),
+                new THREE.Vector3(66, y, -4724 ),
+                new THREE.Vector3(6, y, -2842 ),
+                new THREE.Vector3(-24, y, -1417 ),
+                new THREE.Vector3(-60, y, -1257 ),
+                new THREE.Vector3(-112, y, 1151 ),
+                new THREE.Vector3(-148, y, 2713 ),
+                new THREE.Vector3(-168, y, 4759 ),
+                new THREE.Vector3(-218, y, 4954 ),
+                new THREE.Vector3(-292, y, 5962 ),
+                new THREE.Vector3(-341, y, 6163 ),
+                new THREE.Vector3(-356, y, 6692 ),
+                new THREE.Vector3(-396, y, 6878 ),
+                new THREE.Vector3(-232, y, 7814 ),
+                new THREE.Vector3(-143, y, 9437 ),
+                new THREE.Vector3(-49, y, 12744 ),
+                new THREE.Vector3(-31, y, 13939 ),
+                new THREE.Vector3(28, y, 14856 ),
+                new THREE.Vector3(46, y, 15454 ),
+                new THREE.Vector3(25, y, 16373 ),
+                new THREE.Vector3(-76, y, 16389 ),
+                new THREE.Vector3(-105, y, 17099 ),
+                new THREE.Vector3(-199, y, 18443 ),
+                new THREE.Vector3(-251, y, 19079 ),
+                new THREE.Vector3(-30, y, 19416 ),
+                new THREE.Vector3(2, y, 20076 ),
+                new THREE.Vector3(9, y, 20535 ),
+                new THREE.Vector3(-13, y, 21407 ),
+                new THREE.Vector3(-3, y, 22692 ),
+                new THREE.Vector3(122, y, 23374 ),
+                new THREE.Vector3(160, y, 24383 ),
+                new THREE.Vector3(197, y, 25000 )              
+        ] );
+ 
+        var otocek1 = new THREE.SplineCurve3( [
+                new THREE.Vector3(32, y, 18855 ),
+                new THREE.Vector3(26, y, 18487 )
+        ] );   
+ 
+       
+ 
+        var otocek2 = new THREE.SplineCurve3( [
+                new THREE.Vector3(224, y, -7406 ),
+                new THREE.Vector3(287, y, -8232 ),
+                new THREE.Vector3(362, y, -9701 ),
+                new THREE.Vector3(405, y, -11168 ),
+                new THREE.Vector3(419, y, -13280 ),
+                new THREE.Vector3(401, y, -14406 ),
+                new THREE.Vector3(362, y, -15941 ),
+                new THREE.Vector3(291, y, -17384 ),
+                new THREE.Vector3(208, y, -17997 ),
+                new THREE.Vector3(148, y, -17311 ),
+                new THREE.Vector3(108, y, -16462 ),
+                new THREE.Vector3(52, y, -15041 ),
+                new THREE.Vector3(30, y, -12214 ),
+                new THREE.Vector3(47, y, -10816 ),
+                new THREE.Vector3(72, y, -9600 ),
+                new THREE.Vector3(100, y, -8819 ),
+                new THREE.Vector3(102, y, -8178 ),
+                new THREE.Vector3(133, y, -7951 ),
+                new THREE.Vector3(200, y, -7526 ),
+                new THREE.Vector3(216, y, -7391 )
+        ] );   
+ 
+ 
+ 
+        var geometryLine = new THREE.Geometry();
+        var geometryLine2 = new THREE.Geometry();
+        var geometryLine3 = new THREE.Geometry();
+ 
+        geometryLine.vertices = pot.getPoints( 50 );
+        geometryLine2.vertices = otocek1.getPoints( 50 );
+        geometryLine3.vertices = otocek2.getPoints( 50 );
+ 
+        var materialLine = new THREE.LineBasicMaterial( { color : 0xf32530 } );
+ 
+        //Create the final Object3d to add to the scene
+        var splineObject = new THREE.Line( geometryLine, materialLine );
+        var splineObject2 = new THREE.Line( geometryLine2, materialLine );
+        var splineObject3 = new THREE.Line( geometryLine3, materialLine );
+        scene.add(splineObject);
+        scene.add(splineObject2);
+        scene.add(splineObject3);
+        allCrates.push(splineObject);
+        allCrates.push(splineObject2);
+        allCrates.push(splineObject3);
+
  
         //############ OVIRE ############
         createObstacle(230, 24200);
@@ -169,6 +337,8 @@ function init() {
         //createObstacle(-70, 11200);
         //createObstacle(-110, 9800);
         //createObstacle(-130, 8200);
+
+
  
  
         
@@ -230,16 +400,9 @@ function init() {
         container.appendChild( stats.domElement );
  
 
-        ///############ SCORE ############ - NOT WORKING
-        divScore = document.createElement("div");
-        divScore.style.width = "100px";
-        divScore.style.height = "100px";
-        divScore.style.color = "red";
-	    divScore.style.top = "100%" ;
-	    divScore.style.right = "100%" ;
-	    divScore.style.zIndex = "99";
-        divScore.textContent = score;
-        container.appendChild(divScore);
+        ///############ SCORE ############
+     	document.getElementById("scoreDiv").innerHTML = score.toString();
+
  
         window.addEventListener( 'resize', onWindowResize, false );
  
@@ -413,21 +576,19 @@ function render() {
         }
  
         // space pritisnemo ko se igra že izvaja - igro ustavimo, ter ponovno izrišemo > resetiramo položaj
-        if(keyboard.pressed("space") && started) {
+        if(keyboard.pressed("space")) {
                 init();
                 started = false;
+                colided = false;
+                finished = false;
                 score = 0;
+                document.getElementById("gameOver").innerHTML = "";
         }
        
  
-        // collision detection:
-        //   determines if any of the rays from the cube's origin to each vertex
-        //              intersects any face of a mesh in the array of target meshes
-        //   for increased collision accuracy, add more vertices to the cube;
-        //              for example, new THREE.CubeGeometry( 64, 64, 64, 8, 8, 8, wireMaterial )
-        //   HOWEVER: when the origin of the ray is within the target mesh, collisions do not occur
+        // collision detection
         var originPoint = cube.position.clone();
- 
+        var tmpIndex = 0;
         for (var vertexIndex = 0; vertexIndex < cube.geometry.vertices.length; vertexIndex++){		
 			var localVertex = cube.geometry.vertices[vertexIndex].clone();
 			var globalVertex = localVertex.applyMatrix4( cube.matrix );
@@ -435,23 +596,33 @@ function render() {
 			
 			var ray = new THREE.Raycaster( originPoint, directionVector.clone().normalize() );
 			var collisionResultsCrate = ray.intersectObjects( allCrates );
-			var ray2 = new THREE.Raycaster( originPoint, directionVector.clone().normalize() );
-			var collisionResultsCoins = ray2.intersectObjects( allCoins );
+			//var ray2 = new THREE.Raycaster( originPoint, directionVector.clone().normalize() );
+			var collisionResultsCoins = ray.intersectObjects( allCoins );
+			//var collisionFinish = ray.intersectObjects(finish[0]);
 
 
 			// zadanem škatlo
-			if ( collisionResultsCrate.length > 0 && collisionResultsCrate[0].distance < directionVector.length() ) {
-				console.log(" ZABOJ ###");
-				score--;
-
-				//console.log(" SCORE:  "+score);
+			// if ( collisionResultsCrate.length > 0 && collisionResultsCrate[0].distance < directionVector.length() && started) {
+			if ( collisionResultsCrate.length > 0 && started) {
+				started = false;
+				colided = true;
+				document.getElementById("gameOver").innerHTML = 'ARRRR YOU SUCK! You\'ve collected '+score.toString()+' coins!';
 			}
 
+			// finish
+			// if ( collisionResultsCrate.length > 0 && started) {
+			// 	started = false;
+			// 	finished = true;;
+			// 	document.getElementById("gameOver").innerHTML = 'ARRRR YOU\'RE THE REAL PIRATE! You\'ve collected '+score.toString()+' coins!';
+			// }
+
 			// zadanem žeton
-			if ( collisionResultsCoins.length > 0 && collisionResultsCoins[0].distance < directionVector.length() ) {
+			//if ( collisionResultsCoins.length > 0 && collisionResultsCoins[0].distance < directionVector.length() ) {
+			if ( collisionResultsCoins.length > 0) {
 				console.log("kovanc ++++++++++++++")
+				tmpIndex = vertexIndex
 				score++;
-				//console.log(" SCORE:  "+score);
+				document.getElementById("scoreDiv").innerHTML = score.toString();
 			}	
 		}      
        
